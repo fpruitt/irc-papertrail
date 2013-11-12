b94043095861cfe27b82696ce1b696bc67a3247d
Laurian Gridinoc <laurian@gridinoc.name>
2013-11-11 18:23:21 -0600

    log messages via winston-papertrail

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
+var winston = require('winston');
 var irc = require('irc');
-var bot = new irc.Client('irc.freenode.net', 'hyperaudiologbot', {
+
+require('winston-papertrail').Papertrail;
+
+var logger = new winston.Logger({
+    transports: [
+        new winston.transports.Papertrail({
+            host: 'logs.papertrailapp.com',
+            port: 12345,
+            logFormat: function(level, message) {
+                // return '<<<' + level + '>>> ' + message;
+                return message;
+            }
+        })
+    ]
+});
+  
+var bot = new irc.Client('irc.freenode.net', 'hyperabot', {
     channels: ['#hyperaudio'],
     port: 6667,
     debug: true
 });
 
 bot.addListener('message', function(from, to, message) {
-    if(  message.indexOf('coffee') > -1) {
+    if (message.indexOf('coffee') > -1) {
         bot.say(to, 'Moar Coffee!!!');
     }
+	
+	logger.info(from + ': ' + message);
 });
